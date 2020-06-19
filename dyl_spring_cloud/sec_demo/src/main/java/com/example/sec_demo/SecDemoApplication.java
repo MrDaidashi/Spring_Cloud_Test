bdaa63d2d6eb19ffd9da07943b7375b13e23dd62
yinglei.dai <yinglei.dai@vcarsystem.com>
2020-06-19 10:40:12 +0800

    spring security

---
+++ dyl_spring_cloud/sec_demo/src/main/java/com/example/sec_demo/SecDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sec_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecDemoApplication.class, args);
+	}
+
+}
